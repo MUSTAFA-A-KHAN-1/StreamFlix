05d21ef2fded0ea5530ceb20bac97db4b686ebe6
Mustafa-a-khan <70553059+MUSTAFA-A-KHAN@users.noreply.github.com>
2026-02-09 19:24:41 +0000

    fix:fix

--- src/components/ui/index.js
+++
@@ -1,6 +0,0 @@
-export { Button } from './Button'
-export { Input, SearchInput } from './Input'
-export { ConfirmModal, ImageModal, Modal, VideoModal } from './Modal'
-export { IMDBRating, RatingBadge, RatingCircle, StarRating } from './Rating'
-export { LoadingSpinner, PageLoader, Skeleton, SkeletonCard, SkeletonCast, SkeletonDetails, SkeletonHero, SkeletonRow } from './Skeleton'
-
--- src/services/index.js
+++
@@ -1,4 +0,0 @@
-export * from './anime'
-export * from './tmdb'
-export * from './youtube'
-
